257c80d3efd00fc4e2348a939bfd6af1abec0740
questionMrk <xczhcvbvcxu@gmail.com>
2024-10-06 19:58:13 -0500

    Update LLD.py
    Removed Unneeded code

--- LLD.py
+++ LLD.py
@@ -6,41 +6,6 @@ import colorama
 import messagebox
 from base64 import b64encode
 
-messagebox.showinfo('Privacy Policy (1/2)', """
-Privacy Policy
-
-Effective Date: October 6, 2024
-
-By using LLD Version 1.2 (“the Service”), you acknowledge and agree to the following terms:
-
-1. No Warranty: The Service is provided "as-is" without any guarantees of accuracy, reliability, or availability.
-   
-2. Limitation of Liability: We are not liable for any direct, indirect, incidental, or consequential damages arising from your use of the Service. You assume all risks associated with its use.
-
-3. Third-Party Links: The Service may contain links to third-party websites. We are not responsible for their content or practices.
-
-4. Changes: This policy may be updated periodically. Please review it regularly for any changes.
-""")
-
-messagebox.showinfo('Code of Conduct (2/2)', """
-Code of Conduct
-
-Effective Date: October 6, 2024
-
-By using LLD Version 1.2 (available at github.com), you agree to adhere to the following Code of Conduct:
-
-1. User Responsibility: You are solely responsible for your actions while using the Service. Inappropriate or illegal behavior is not endorsed or condoned.
-
-2. Prohibited Activities: Engaging in activities that may harm others, including harassment, abuse, or illegal actions, is strictly prohibited.
-
-3. No Liability: We are not responsible for consequences arising from your actions while using the Service. Violations of this Code are solely your responsibility.
-
-4. Reporting Violations: Please report any violations of this Code of Conduct to us at [Your Email Address].
-
-5. Changes to the Code: This Code of Conduct may be updated periodically. Please review it regularly for any changes.
-
-By using the Service, you acknowledge your understanding and acceptance of this Code of Conduct.
-""")
 def MenuCustom(text): # to remove this nasty ass code
     print(f"""
 {colorama.Fore.LIGHTCYAN_EX}╔══════════════════════════════════════════════════════════════════╗
